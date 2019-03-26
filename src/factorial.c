7c4bf635c00784b5ef1e39e6a946ae75eb354dbb
XLiu <liu.xu@sufe.edu.cn>
2019-03-26 11:01:30 +0800

    updated

--- src/factorial.c
+++ src/factorial.c
@@ -222,10 +222,10 @@ SEXP fact_sum(SEXP N)
 		SET_STRING_ELT(rfact_sum, 0,  mkChar(fact_sum));		
 	}
 	else{	
-		int j,L, curr_len, Ln=2;
+		int j,L, curr_len, Ln=1;
 		i = n;
 		while(i/10){i = i/10; Ln++;	}
-		L=(Ln-2+i*0.1)*n;
+		L=(Ln-1+i*0.1)*n;
 		fact_n = (int *)malloc(sizeof(int)*L);
 		int *bb = (int*) malloc(sizeof(int)*L);
 
